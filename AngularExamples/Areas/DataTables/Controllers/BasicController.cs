61b98a89b00c0c5fdc139a0821d19e9265358b66
Kiếm Phong <Anhph68@gmail.com>
2015-01-20 16:56:28 +0700

    Angular-Datatable work with server side

--- AngularExamples/Areas/DataTables/Controllers/BasicController.cs
+++ AngularExamples/Areas/DataTables/Controllers/BasicController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using AngularExamples.Areas.DataTables.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace AngularExamples.Areas.DataTables.Controllers
 {
@@ -8,6 +13,93 @@ namespace AngularExamples.Areas.DataTables.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Basic()
+        {
+            return PartialView();
+        }
+
+        public ActionResult LoadJson()
+        {
+            return PartialView();
+        }
+
+        public ActionResult AppList()
+        {
+            return PartialView();
+        }
+
+        private TDKTEntities db = new TDKTEntities();
+
+        public ActionResult GetAppList(DataTableParamModel param)
+        {
+            IEnumerable<tblApp> allResult = db.tblApps.ToList();
+
+            IEnumerable<tblApp> filtered;
+            //Check whether the companies should be filtered by keyword
+            //if (!string.IsNullOrEmpty(param.sSearch))
+            //{
+            //    //Optionally check whether the columns are searchable at all 
+            //    var search0 = Convert.ToBoolean(Request["bSearchable_0"]);
+            //    var search1 = Convert.ToBoolean(Request["bSearchable_1"]);
+            //    var search2 = Convert.ToBoolean(Request["bSearchable_2"]);
+            //    int tmp = int.TryParse(param.sSearch, out tmp) ? tmp : 0;
+
+            //    filtered = allResult
+            //        .Where(c => search1 && c.AppName.ToLower().Contains(param.sSearch.ToLower())
+            //                 || search2 && c.AppUrl.ToLower().Contains(param.sSearch.ToLower())
+            //                 || search0 && c.Id.Equals(tmp)
+            //         );
+            //}
+            //else filtered = allResult;
+
+            var tmpCount = allResult.Count();
+
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                //Optionally check whether the columns are searchable at all 
+                var search0 = Convert.ToBoolean(Request["bSearchable_0"]);
+                var search1 = Convert.ToBoolean(Request["bSearchable_1"]);
+                var search2 = Convert.ToBoolean(Request["bSearchable_2"]);
+                int tmp = int.TryParse(param.sSearch, out tmp) ? tmp : 0;
+
+                allResult = allResult
+                    .Where(c => search1 && c.AppName.ToLower().Contains(param.sSearch.ToLower())
+                             || search2 && c.AppUrl.ToLower().Contains(param.sSearch.ToLower())
+                             || search0 && c.Id.Equals(tmp)
+                     );
+            }
+
+            //var sort0 = Convert.ToBoolean(Request["bSortable_0"]);
+            //var sort1 = Convert.ToBoolean(Request["bSortable_1"]);
+            //var sort2 = Convert.ToBoolean(Request["bSortable_2"]);
+            //var sortColumnIndex = Convert.ToInt64(Request["iSortCol_0"]);
+
+            //Func<tblApp, string> orderingFunction = (c => sortColumnIndex == 1 && sort1 ? c.AppName :
+            //                                                sortColumnIndex == 2 && sort2 ? c.AppUrl : "");
+            //Func<tblApp, int> orderingFunction2 = (c => sortColumnIndex == 0 && sort0 ? c.Id : 0);
+
+            //var sortDirection = Request["sSortDir_0"]; // asc or desc
+            //filtered = sortDirection == "asc" ? filtered.OrderBy(orderingFunction).ThenBy(orderingFunction2) : filtered.OrderByDescending(orderingFunction).ThenByDescending(orderingFunction2);
+
+            //var displayed = filtered.Skip(param.iDisplayStart).Take(param.iDisplayLength);
+
+
+            var result = allResult.Select(c => new
+            {
+                col0 = c.Id,
+                col1 = c.AppName,
+                col2 = c.AppUrl
+            });
+
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = allResult.Count(),
+                iTotalDisplayRecords = allResult.Count(),
+                aaData = result
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ AngularExamples/Scripts/apps/datatables/datatable.js
@@ -0,0 +1,83 @@
+﻿var app = angular.module("myApp", ["ui.router", "datatables", "angular-loading-bar"]);
+app.config(routeConfig)
+    .config(function (cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.includeSpinner = false;
+    });
+
+function routeConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/basic");
+    $stateProvider
+        .state("basic", {
+            url: "/basic",
+            views: {
+                '': {
+                    templateUrl: "Basic",
+                    controller: "mainCtrl"
+                },
+                'json@basic': {
+                    templateUrl: "LoadJson",
+                    controller: "loadJsonCtrl"
+                }
+            }
+        })
+        .state("json", {
+            url: "/json",
+            templateUrl: "LoadJson",
+            controller: 'loadJsonCtrl'
+        })
+        .state("apps", {
+            url: "/apps",
+            templateUrl: "AppList",
+            controller: 'AppsCtrl'
+        });
+};
+
+app.controller("mainCtrl", function (DTOptionsBuilder, DTColumnDefBuilder) {
+    var vm = this;
+    vm.dtOptions = DTOptionsBuilder.newOptions()
+        .withPaginationType("full_numbers")
+        //.withDisplayLength(2)
+        .withBootstrap();
+
+    vm.dtColumnDefs = [
+        DTColumnDefBuilder.newColumnDef(0),
+        DTColumnDefBuilder.newColumnDef(1),
+        DTColumnDefBuilder.newColumnDef(2).notSortable()
+    ];
+});
+
+app.controller("loadJsonCtrl", function (DTOptionsBuilder, DTColumnBuilder) {
+    var vm = this;
+    vm.dtOptions = DTOptionsBuilder
+        .fromSource('../../Scripts/apps/data.json')
+        .withPaginationType('full_numbers')
+        .withBootstrap();
+    vm.dtColumns = [
+        DTColumnBuilder.newColumn('id').withTitle('ID').withClass('col-md-1 text-left'),
+        DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+        DTColumnBuilder.newColumn('lastName').withTitle('Last name')//.notVisible()
+    ];
+});
+
+app.controller("AppsCtrl", function (DTOptionsBuilder, DTColumnBuilder) {
+    var vm = this;
+    vm.dtOptions = DTOptionsBuilder
+        //.fromSource('GetAppList')
+        .newOptions()
+        .withOption('ajax', {
+            // Either you specify the AjaxDataProp here
+            // dataSrc: 'data',
+            url: 'GetAppList',
+            type: 'POST'
+        })
+        // or here
+        .withDataProp('aaData')
+        .withOption('serverSide', true)
+        .withPaginationType('full_numbers')
+        .withBootstrap();
+    vm.dtColumns = [
+        DTColumnBuilder.newColumn('col0').withTitle('Id').withClass('col-md-1'),
+        DTColumnBuilder.newColumn('col1').withTitle('Name of Application'),
+        DTColumnBuilder.newColumn('col2').withTitle('Url').withClass('col-md-2')
+    ];
+});
